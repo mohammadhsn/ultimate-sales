07bc053fdffb5971963bd2460d24a4320944778e
Mohammad <m.hoseini@samincoin.com>
2022-10-01 17:07:26 +0330

    setup logger

--- app/services/sales/main.go
+++ app/services/sales/main.go
@@ -1,25 +1,48 @@
 package main
 
 import (
-	_ "go.uber.org/automaxprocs"
-	"log"
+	"fmt"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"os"
-	"os/signal"
-	"runtime"
-	"syscall"
 )
 
 var build = "develop"
 
 func main() {
-	g := runtime.GOMAXPROCS(0)
+	// Construct the application logger.
+	log, err := initLogger("SALES")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-	log.Printf("starting service [%s], CPU [%d]", build, g)
-	defer log.Println("service ended")
+	defer log.Sync()
+	// Perform the startup and shutdown sequence.
+	if err := run(log); err != nil {
+		log.Errorw("startup", "ERROR", err)
+		os.Exit(1)
+	}
+}
 
-	shutdown := make(chan os.Signal, 1)
-	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
-	<-shutdown
+func run(log *zap.SugaredLogger) error {
+	return nil
+}
 
-	log.Println("stopping service")
+func initLogger(service string) (*zap.SugaredLogger, error) {
+	config := zap.NewProductionConfig()
+	config.OutputPaths = []string{"stdout"}
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.DisableStacktrace = true
+	config.InitialFields = map[string]interface{}{
+		"service": service,
+	}
+
+	log, err := config.Build()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return log.Sugar(), nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/mohammadhsn/ultimate-service
 
 go 1.18
 
-require go.uber.org/automaxprocs v1.5.1
+require go.uber.org/zap v1.23.0
+
+require (
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+)
